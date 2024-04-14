cbcdb1aa6db7ff8a3aa2be8701c51d1406cc802c
“Santiago” <santimedinaok.10@gmail.com>
2024-04-14 19:30:19 -0300

    Punto 3 completado

--- tp2_3.c
+++ tp2_3.c
@@ -1,17 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define N 5
 #define M 7
 
 int main(){
 
     int i,j;
-    int mt[N][M];
+    float mt[N][M];
+    float *puntero_mt = &mt[0][0]; 
 
     for(i = 0;i<N; i++)
         {
             for(j = 0;j<M; j++)
                 {
-                    mt[i][j]=1+rand()%100;
-                    printf("%lf", mt[i][j]);
+                    *(puntero_mt + i * M + j) = 1 + rand() % 100;
+
+                    printf("%f \n", *(puntero_mt + i * M + j));
                 }
             printf("\n");
         }
